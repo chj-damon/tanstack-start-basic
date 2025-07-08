e70065587aa3652308776c744bfeb8aa7d0dadad
damon.chen <chj_0507_dz@sina.com>
2025-07-08 18:14:03 +0800

    fix: 修改axios的使用bug

--- src/utils/users.tsx
+++ src/utils/users.tsx
@@ -1,5 +1,5 @@
 import { queryOptions } from "@tanstack/react-query"
-import axios from 'redaxios';
+import api from '@/utils/api';
 
 export type User = {
   id: number
@@ -9,14 +9,14 @@ export type User = {
 
 export const usersQueryOptions = () => queryOptions({
   queryKey: ['users'],
-  queryFn: () => axios.get<Array<User>>('/api/users').then(res => res.data).catch(() => {
+  queryFn: () => api.get<Array<User>>('/api/users').then(res => res.data).catch(() => {
     throw new Error('Unexpected status code')
   })
 })
 
 export const userQueryOptions = (userId: string) => queryOptions({
   queryKey: ['users', userId],
-  queryFn: () => axios.get<User>(`/api/users/${userId}`).then(res => res.data).catch(() => {
+  queryFn: () => api.get<User>(`/api/users/${userId}`).then(res => res.data).catch(() => {
     throw new Error('Unexpected status code')
   })
 });
